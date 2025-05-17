2b293358e309a00bd6b188d0db4d3e7479aea6d3
NayrGosling <laletin.dmitrij2015@yandex.ru>
2025-05-17 11:49:45 +0300

    fix

--- internal/handler/person.go
+++ internal/handler/person.go
@@ -51,8 +51,8 @@ func (h *PersonHandler) Create(w http.ResponseWriter, r *http.Request) {
 	)
 
 	query := `
-        INSERT INTO persons (name, surname, patronymic, age, gender, nationality)
-        VALUES ($1,$2,$3,$4,$5,$6) RETURNING id, created_at`
+		INSERT INTO persons (name, surname, patronymic, age, gender, nationality)
+		VALUES ($1,$2,$3,$4,$5,$6) RETURNING id, created_at`
 	log.Debug("PersonHandler.Create: executing DB insert")
 	if err := h.DB.QueryRow(query,
 		p.Name, p.Surname, p.Patronymic, p.Age, p.Gender, p.Nationality,
@@ -64,26 +64,15 @@ func (h *PersonHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 	log.Infof("PersonHandler.Create: created person ID=%d", p.ID)
 	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(p); err != nil {
-		log.WithError(err).Warn("PersonHandler.Create: failed to write response")
-	}
-}
-
-func ptrToString[T any](p *T, def string) string {
-	if p == nil {
-		return def
-	}
-	return fmt.Sprint(*p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func (h *PersonHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 	log.Debug("PersonHandler.GetAll: parsing query params")
 	q := r.URL.Query()
-	var (
-		where []string
-		args  []interface{}
-		idx   = 1
-	)
+	var where []string
+	var args []interface{}
+	idx := 1
 	if v := q.Get("name"); v != "" {
 		where = append(where, fmt.Sprintf("name ILIKE $%d", idx))
 		args = append(args, "%"+v+"%")
@@ -96,8 +85,8 @@ func (h *PersonHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 	}
 
 	base := `
-        SELECT id, name, surname, patronymic, age, gender, nationality, created_at
-        FROM persons`
+		SELECT id, name, surname, patronymic, age, gender, nationality, created_at
+		FROM persons`
 	if len(where) > 0 {
 		base += " WHERE " + strings.Join(where, " AND ")
 	}
@@ -120,14 +109,8 @@ func (h *PersonHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 	var result []model.Person
 	for rows.Next() {
 		var p model.Person
-		if err := rows.Scan(
-			&p.ID, &p.Name, &p.Surname, &p.Patronymic,
-			&p.Age, &p.Gender, &p.Nationality, &p.CreatedAt,
-		); err != nil {
-			log.WithError(err).Error("PersonHandler.GetAll: failed scanning row")
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		rows.Scan(&p.ID, &p.Name, &p.Surname, &p.Patronymic,
+			&p.Age, &p.Gender, &p.Nationality, &p.CreatedAt)
 		fullName := p.Name + " " + p.Surname
 		if p.Patronymic != nil {
 			fullName += " " + *p.Patronymic
@@ -144,9 +127,45 @@ func (h *PersonHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 
 	log.Infof("PersonHandler.GetAll: returning %d persons", len(result))
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(result); err != nil {
-		log.WithError(err).Warn("PersonHandler.GetAll: failed to write response")
+	json.NewEncoder(w).Encode(result)
+}
+
+func (h *PersonHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/persons/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.WithError(err).Warnf("PersonHandler.GetByID: invalid id %s", idStr)
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
 	}
+	log.Infof("PersonHandler.GetByID: fetching person id=%d", id)
+	var p model.Person
+	err = h.DB.QueryRow(
+		`SELECT id, name, surname, patronymic, age, gender, nationality, created_at FROM persons WHERE id=$1`,
+		id,
+	).Scan(&p.ID, &p.Name, &p.Surname, &p.Patronymic,
+		&p.Age, &p.Gender, &p.Nationality, &p.CreatedAt)
+	if err == sql.ErrNoRows {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		log.WithError(err).Error("PersonHandler.GetByID: query failed")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fullName := p.Name + " " + p.Surname
+	if p.Patronymic != nil {
+		fullName += " " + *p.Patronymic
+	}
+	p.Message = fmt.Sprintf(
+		"%s: age %s, gender %s, nationality %s",
+		fullName,
+		ptrToString(p.Age, "unknown"),
+		ptrToString(p.Gender, "unknown"),
+		ptrToString(p.Nationality, "unknown"),
+	)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(p)
 }
 
 func (h *PersonHandler) Update(w http.ResponseWriter, r *http.Request) {
@@ -164,13 +183,19 @@ func (h *PersonHandler) Update(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	query := `UPDATE persons SET name=$1,surname=$2,patronymic=$3 WHERE id=$4`
-	if _, err := h.DB.Exec(query, p.Name, p.Surname, p.Patronymic, id); err != nil {
+	res, err := h.DB.Exec(
+		`UPDATE persons SET name=$1, surname=$2, patronymic=$3 WHERE id=$4`,
+		p.Name, p.Surname, p.Patronymic, id,
+	)
+	if err != nil {
 		log.WithError(err).Error("PersonHandler.Update: exec failed")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	log.Infof("PersonHandler.Update: updated person id=%d", id)
+	if n, _ := res.RowsAffected(); n == 0 {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
 	w.WriteHeader(http.StatusNoContent)
 }
 
@@ -183,11 +208,22 @@ func (h *PersonHandler) Delete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Infof("PersonHandler.Delete: deleting person id=%d", id)
-	if _, err := h.DB.Exec("DELETE FROM persons WHERE id=$1", id); err != nil {
-		log.WithError(err).Error("PersonHandler.Delete: delete failed")
+	res, err := h.DB.Exec("DELETE FROM persons WHERE id=$1", id)
+	if err != nil {
+		log.WithError(err).Error("PersonHandler.Delete: exec failed")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	log.Infof("PersonHandler.Delete: deleted person id=%d", id)
+	if n, _ := res.RowsAffected(); n == 0 {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
 	w.WriteHeader(http.StatusNoContent)
+}
+
+func ptrToString[T any](p *T, def string) string {
+	if p == nil {
+		return def
+	}
+	return fmt.Sprint(*p)
 }
